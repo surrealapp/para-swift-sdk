1729f0b16cc73d1bfcca7534c7807ac1b78d10e3
Brian Corbin <brian.william.corbin@gmail.com>
2024-06-25 13:52:54 -0700

    Revert "updating main actor stuff"
    This reverts commit 70e15dd573150ecce5a0a17472b2034437b2ae36.

--- Sources/CapsuleSwift/Capsule.swift
+++ Sources/CapsuleSwift/Capsule.swift
@@ -22,6 +22,7 @@ import os
 ///     ...
 /// ```
 @available(iOS 16.4, *)
+@MainActor
 public struct CapsuleWebView: UIViewRepresentable {
     
     /// An instance of the Capsule class
@@ -51,28 +52,15 @@ public struct CapsuleWebView: UIViewRepresentable {
     }
 }
 
-enum CapsuleError: Error {
-    case bridgeError(String)
-    case bridgeInUseError
-    case bridgeTimeoutError
-}
-
-extension CapsuleError: CustomStringConvertible {
-    var description: String {
-        switch self {
-        case .bridgeError(let info):
-            return "The following error happened while the javascript bridge was executing: \(info)"
-        case .bridgeInUseError:
-            return "The javascript bridge is currently processing a request. Only one request may be triggered at a time."
-        case .bridgeTimeoutError:
-            return "The javascript bridge did not respond in time and the continuation has been cancelled."
-        }
-    }
-}
-
 @available(iOS 16.4, *)
+@MainActor
 public class Capsule: NSObject, ObservableObject, WKNavigationDelegate, WKScriptMessageHandler {
     public static let packageVersion = "0.0.2"
+        
+    enum CapsuleError: Error {
+        case missingResponseData
+        case bridgeError(String)
+    }
     
     private var continuation: CheckedContinuation<Any?, Error>?
     private let passkeysManager: PasskeysManager
@@ -103,12 +91,10 @@ public class Capsule: NSObject, ObservableObject, WKNavigationDelegate, WKScript
         
         if let error = resp["error"] {
             continuation?.resume(throwing: CapsuleError.bridgeError(error as? String ?? "No further information provided"))
-            continuation = nil
             return
         }
         
         continuation?.resume(returning: resp["responseData"])
-        continuation = nil
     }
     
     public func loadJsBridge() {
@@ -133,12 +119,7 @@ public class Capsule: NSObject, ObservableObject, WKNavigationDelegate, WKScript
     """)
     }
     
-    @MainActor
     private func postMessage(method: String, arguments: [Encodable]) async throws -> Any? {
-        if let _ = self.continuation {
-            throw CapsuleError.bridgeInUseError
-        }
-        
         return try await withCheckedThrowingContinuation { continuation in
             self.continuation = continuation
             webView!.evaluateJavaScript("""
@@ -154,19 +135,16 @@ public class Capsule: NSObject, ObservableObject, WKNavigationDelegate, WKScript
 
 @available(iOS 16.4, *)
 extension Capsule {
-    @MainActor
     public func checkIfUserExists(email: String) async throws -> Bool {
         let result = try await postMessage(method: "checkIfUserExists", arguments: [email])
         return result as! Bool
     }
     
-    @MainActor
     public func createUser(email: String) async throws {
         try await postMessage(method: "createUser", arguments: [email])
         return
     }
     
-    @MainActor
     public func login(authorizationController: AuthorizationController) async throws {
         let getWebChallengeResult = try await postMessage(method: "getWebChallenge", arguments: [])
         let challenge = (getWebChallengeResult as! [String: String])["challenge"]!
@@ -184,7 +162,6 @@ extension Capsule {
         self.wallet = Wallet(result: (wallet as! [String: Any]))
     }
     
-    @MainActor
     public func verify(verificationCode: String) async throws -> String {
         let result = try await postMessage(method: "verifyEmail", arguments: [verificationCode])
         let paths = (result as! String).split(separator: "/")
@@ -192,7 +169,6 @@ extension Capsule {
         return String(biometricsId)
     }
     
-    @MainActor
     public func generatePasskey(email: String, biometricsId: String, authorizationController: AuthorizationController) async throws {
         var userHandle = Data(count: 32)
         let _ = userHandle.withUnsafeMutableBytes {
@@ -209,47 +185,39 @@ extension Capsule {
         try await postMessage(method: "generatePasskeyV2", arguments: [attestationObjectEncoded, clientDataJSONEncoded, credentialIDEncoded, userHandleEncoded, biometricsId])
     }
     
-    @MainActor
     public func setup2FA() async throws -> String {
         let result = try await postMessage(method: "setup2FA", arguments: [])
         return (result as! [String: String])["uri"]!
     }
     
-    @MainActor
     public func enable2FA() async throws {
         let result = try await postMessage(method: "enable2FA", arguments: [])
     }
 
-    @MainActor
     public func is2FASetup() async throws -> Bool {
         let result = try await postMessage(method: "check2FAStatus", arguments: [])
         return (result as! [String: Any])["isSetup"]! as! Bool
     }
     
-    @MainActor
     public func resendVerificationCode() async throws {
         try await postMessage(method: "resendVerificationCode", arguments: [])
     }
     
-    @MainActor
     public func isFullyLoggedIn() async throws -> Bool {
         let result = try await postMessage(method: "isFullyLoggedIn", arguments: [])
         return result as! Bool
     }
 
-    @MainActor
     public func isSessionActive() async throws -> Bool {
         let result = try await postMessage(method: "isSessionActive", arguments: [])
         return result as? Bool ?? false
     }
     
-    @MainActor
     public func exportSession() async throws {
         let result = try await postMessage(method: "exportSession", arguments: [])
         print(result)
     }
     
-    @MainActor
     public func logout() async throws {
         try await postMessage(method: "logout", arguments: [])
         wallet = nil
@@ -258,21 +226,18 @@ extension Capsule {
 
 @available(iOS 16.4, *)
 extension Capsule {
-    @MainActor
     public func createWallet(skipDistributable: Bool) async throws {
         let result = try await postMessage(method: "createWallet", arguments: [skipDistributable])
         let walletAndRecovery = (result as! [[String: Any]])[0]
         self.wallet = Wallet(result: (walletAndRecovery["wallet"] as! [String: String]))
     }
     
-    @MainActor
     public func fetchWallets() async throws -> [Wallet] {
         let result = try await postMessage(method: "fetchWallets", arguments: [])
         let wallets = (result as! [[String: Any]]).map { Wallet(result: $0)}
         return wallets
     }
     
-    @MainActor
     public func distributeNewWalletShare(walletId: String, userShare: String) async throws {
         let result = try await postMessage(method: "distributeNewWalletShare", arguments: [walletId, userShare])
         print(result)
@@ -281,20 +246,17 @@ extension Capsule {
 
 @available(iOS 16.4, *)
 extension Capsule {
-    @MainActor
     public func signMessage(walletId: String, message: String) async throws -> String {
         let result = try await postMessage(method: "signMessage", arguments: [walletId, message.toBase64()])
         return (result as! [String: String])["signature"]!
     }
     
-    @MainActor
     public func signTransaction(walletId: String, rlpEncodedTx: String, chainId: String) async throws -> String {
         let result = try await postMessage(method: "signTransaction", arguments: [walletId, rlpEncodedTx.toBase64(), chainId])
         
         return (result as! [String: String])["signature"]!
     }
     
-    @MainActor
     public func sendTransaction(walletId: String, rlpEncodedTx: String, chainId: String) async throws -> String {
         let result = try await postMessage(method: "sendTransaction", arguments: [walletId, rlpEncodedTx.toBase64(), chainId])
         
