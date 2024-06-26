423a29fc3283ae253c799e6f7adb235a9cd8b237
Joe Blau <im@joeblau.com>
2024-06-25 20:48:27 -0700

    feat: update names

---
+++ Sources/CapsuleSwift/CapsuleSessionState.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joe Blau on 6/25/24.
+//
+
+import Foundation
+
+public enum CapsuleSessionState: Int {
+    case unknown
+    case inactive
+    case active
+    case activeLoggedIn
+}
