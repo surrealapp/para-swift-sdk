a8649aac532cf3d445310811aba4ff587510ad5f
Jordy McNab <jordy@usecapsule.com>
2024-12-20 19:11:57 -0600

    chore: modify to allow env

--- example/example/exampleApp.swift
+++ example/example/exampleApp.swift
@@ -1,26 +1,48 @@
-//
-//  exampleApp.swift
-//  example
-//
-//  Created by Brian Corbin on 6/18/24.
-//
-
 import SwiftUI
 import CapsuleSwift
 
 @main
 struct exampleApp: App {
-    @StateObject private var capsuleManager = CapsuleManager(environment: .sandbox, apiKey: "8ee2d015fbc6062a6e30bdc472f2946c")
+    @StateObject private var capsuleManager: CapsuleManager
     @StateObject private var appRootManager = AppRootManager()
+    
+    init() {
+        let environmentString = ProcessInfo.processInfo.environment["ENVIRONMENT"] ?? "beta"
+        
+        let environment: CapsuleEnvironment = environmentString == "sandbox" ? .sandbox : .beta
+        
+        let sandboxApiKey = ProcessInfo.processInfo.environment["CAPSULE_SANDBOX_API_KEY"]
+        let betaApiKey = ProcessInfo.processInfo.environment["CAPSULE_BETA_API_KEY"]
+        
+        let apiKey: String
+        if environment == .sandbox {
+            guard let key = sandboxApiKey, !key.isEmpty else {
+                fatalError("CAPSULE_SANDBOX_API_KEY not found or empty in environment variables.")
+            }
+            apiKey = key
+        } else {
+            guard let key = betaApiKey, !key.isEmpty else {
+                fatalError("CAPSULE_BETA_API_KEY not found or empty in environment variables.")
+            }
+            apiKey = key
+        }
+        
+        _capsuleManager = StateObject(wrappedValue: CapsuleManager(environment: environment, apiKey: apiKey))
+    }
     
     var body: some Scene {
         WindowGroup {
             switch appRootManager.currentRoot {
             case .authentication:
-                UserAuthView().environmentObject(capsuleManager).environmentObject(appRootManager)
+                UserAuthView()
+                    .environmentObject(capsuleManager)
+                    .environmentObject(appRootManager)
             case .home:
-                WalletView().environmentObject(capsuleManager).environmentObject(appRootManager)
+                WalletView()
+                    .environmentObject(capsuleManager)
+                    .environmentObject(appRootManager)
             }
         }
     }
 }
+
